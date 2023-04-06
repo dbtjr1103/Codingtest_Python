3c0ed41b8becdf2a8f5e45445cf8e59d98b988eb
Yuseok, Jung <115054808+dbtjr1103@users.noreply.github.com>
2023-04-06 16:55:52 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -1,11 +1,3 @@
 def solution(number):
-    
-    from itertools import combinations
-
-    trio = list(combinations(number, 3))
-    count = 0
-    for i in trio:
-        if sum(i) == 0:
-            count += 1
-    
-    return count
+    answer = 0
+    return answer
