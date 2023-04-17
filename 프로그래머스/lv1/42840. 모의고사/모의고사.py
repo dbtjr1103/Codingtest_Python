c1971ae75940ccffd6b94c3b83d9ac2fada158c2
Yuseok, Jung <115054808+dbtjr1103@users.noreply.github.com>
2023-04-17 23:33:13 +0900

    [level 1] Title: 모의고사, Time: 3.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.py
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -2,21 +2,19 @@ def solution(answers):
     pattern1 = [1,2,3,4,5]
     pattern2 = [2,1,2,3,2,4,2,5]
     pattern3 = [3,3,1,1,2,2,4,4,5,5]
-    
     score = [0, 0, 0]
-    
-    for i in range(len(answers)):
-        if answers[i] == pattern1[i%len(pattern1)]:
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
             score[0] += 1
-        if answers[i] == pattern2[i%len(pattern2)]:
+        if answer == pattern2[idx%len(pattern2)]:
             score[1] += 1
-        if answers[i] == pattern3[i%len(pattern3)]:
+        if answer == pattern3[idx%len(pattern3)]:
             score[2] += 1
-    
-    max_score = max(score)
-    answer = []
-    for i in range(len(score)):
-        if score[i] == max_score:
-            answer.append(i+1)
-    
-    return answer
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
