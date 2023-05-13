b0306ae6a5bfbb1d02bb792d4446090906600f81
Yuseok, Jung <115054808+dbtjr1103@users.noreply.github.com>
2023-05-13 23:25:12 +0900

    [unrated] Title: 과일 장수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,19 +1,2 @@
 def solution(k, m, score):
-    score.sort(reverse=True)
-    apples_per_box = m
-    boxes_sold = 0
-    max_profit = 0
-    i = 0
-
-    while i < len(score):
-        box_scores = score[i:i+apples_per_box]
-        if not box_scores:
-            break
-        lowest_score = box_scores[-1]
-        i += apples_per_box
-        if len(box_scores) < apples_per_box:
-            continue
-        boxes_sold += 1
-        max_profit += lowest_score * apples_per_box
-
-    return max_profit
+    return sum(sorted(score)[len(score)%m::m])*m
